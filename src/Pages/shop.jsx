6b24a35a231380e2aa6279705ce916adc6152782
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-11-28 18:24:22 +0330

    Update shop.jsx

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import Hero from '../Components/Hero/Hero'
+import Heroo from '../Components/Hero/Heroo';
 import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+
+
 
 const shop = () => {
   return (
     <div>
-      <Hero/>
+      <Heroo/>
       <Popular/>
+      <Offers/>
+      <NewCollections />
     </div>
   )
 }
 
 export default shop;
-
-
