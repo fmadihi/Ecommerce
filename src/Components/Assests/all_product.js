ad5e66d2d2c482241b70ecaba7c385d31e1ae1ba
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-11-28 18:20:43 +0330

    Add files via upload

---
+++ src/Components/Assests/all_product.js
@@ -0,0 +1,95 @@
+import p1_img from "./pic-001.jpg";
+import p2_img from "./pic-002.jpg";
+import p3_img from "./pic-003.jpg";
+import p4_img from "./pic-004.jpg";
+import p5_img from "./pic-005.jpg";
+import p6_img from "./pic-006.jpg";
+import p7_img from "./pic-007.jpg";
+import p8_img from "./pic-008.jpg";
+import p9_img from "./pic-009.jpg";
+import p10_img from "./pic-010.jpg";
+
+let all_product = [
+    {
+        id: 1,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p1_img,
+        new_price: 80.0,
+        old_price: 100.0,
+    },
+    {
+        id: 2,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p2_img,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 3,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p3_img,
+        new_price: 120.0,
+        old_price: 150.0,
+    },
+    {
+        id: 4,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p4_img,
+        new_price: 80.0,
+        old_price: 100.0,
+    },
+    {
+        id: 5,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p5_img,
+        new_price: 60.0,
+        old_price: 100.0,
+    },
+    {
+        id: 6,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p6_img,
+        new_price: 70.0,
+        old_price: 90.0,
+    },
+    {
+        id: 7,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p7_img,
+        new_price: 80.0,
+        old_price: 110.0,
+    },
+    {
+        id: 8,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p8_img,
+        new_price: 30.0,
+        old_price: 40.0,
+    },
+    {
+        id: 9,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p9_img,
+        new_price: 50.0,
+        old_price: 70.0,
+    },
+    {
+        id: 10,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p10_img,
+        new_price: 100.0,
+        old_price: 140.0,
+    },
+    
+]
+export default data_product
---
+++ src/Components/Assests/new_collection.js
@@ -0,0 +1,95 @@
+import p1_img from "./new-col01.jpg";
+import p2_img from "./new-col02.jpg";
+import p3_img from "./new-col03.jpg";
+import p4_img from "./new-col04.jpg";
+import p5_img from "./new-col05.jpg";
+import p6_img from "./new-col01.jpg";
+import p7_img from "./new-col02.jpg";
+import p8_img from "./new-col03.jpg";
+import p9_img from "./new-col04.jpg";
+import p10_img from "./new-col05.jpg";
+
+let data_product = [
+    {
+        id: 1,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p1_img,
+        new_price: 80.0,
+        old_price: 100.0,
+    },
+    {
+        id: 2,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p2_img,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 3,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p3_img,
+        new_price: 120.0,
+        old_price: 150.0,
+    },
+    {
+        id: 4,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p4_img,
+        new_price: 80.0,
+        old_price: 100.0,
+    },
+    {
+        id: 5,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p5_img,
+        new_price: 60.0,
+        old_price: 100.0,
+    },
+    {
+        id: 6,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p6_img,
+        new_price: 70.0,
+        old_price: 90.0,
+    },
+    {
+        id: 7,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p7_img,
+        new_price: 80.0,
+        old_price: 110.0,
+    },
+    {
+        id: 8,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p8_img,
+        new_price: 30.0,
+        old_price: 40.0,
+    },
+    {
+        id: 9,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p9_img,
+        new_price: 50.0,
+        old_price: 70.0,
+    },
+    {
+        id: 10,
+        name: "shirt scarf t_shirt pants",
+        category: "women",
+        image: p10_img,
+        new_price: 100.0,
+        old_price: 140.0,
+    },
+    
+]
+export default data_product
