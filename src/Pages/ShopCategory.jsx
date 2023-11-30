0512a76ff3cfecd4520724cfbf746f4f62b1fe87
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-11-30 15:08:20 +0330

    Update and rename shopCategory.jsx to ShopCategory.jsx

---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assests/dropdown.png'
+import Item from '../Components/Item/Item'
+
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className='shop-category'>
+      <div className="shop-category-left">
+        <h2>FLAT 50% OFF</h2>
+        <div>
+          <p><span>12</span> Hours <span>20</span> Mins</p>
+        </div>
+        <div className="shop-category-btn">
+          <div>Explore now</div>
+        </div>
+      </div>
+      <div className="shop-category-right">
+        <img src={props.banner} alt="" />
+        </div>
+        <div className="shopcategory-indexSort">
+          <p>
+            <span>Showing 1-12</span>out of 36 products
+          </p>
+          <div className="shopcategory-sort">
+            <img src={dropdown_icon} alt="" />
+          </div>
+        </div>
+        <div className="shopcategory-product">
+          {all_product.map((item, i) => {
+            if (props.category === item.category) {
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            }
+            else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+  )
+}
+
+export default ShopCategory
--- src/Pages/shopCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const shopCategory = () => {
-  return (
-    <div>shopCategory</div>
-  )
-}
-
-export default shopCategory
