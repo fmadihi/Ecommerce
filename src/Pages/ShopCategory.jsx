0208c88dde9e75f8b5b5f329cb8d32043e1cef33
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-03 20:17:08 +0330

    Update ShopCategory.jsx

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -9,6 +9,7 @@ const ShopCategory = (props) => {
   return (
     <div className='shop-category'>
       <div className="shop-category-left">
+        <div>
         <h2>FLAT 50% OFF</h2>
         <div>
           <p><span>12</span> Hours <span>20</span> Mins</p>
@@ -16,30 +17,6 @@ const ShopCategory = (props) => {
         <div className="shop-category-btn">
           <div>Explore now</div>
         </div>
-      </div>
-      <div className="shop-category-right">
-        <img src={props.banner} alt="" />
-        </div>
-        <div className="shopcategory-indexSort">
-          <p>
-            <span>Showing 1-12</span>out of 36 products
-          </p>
-          <div className="shopcategory-sort">
-            <img src={dropdown_icon} alt="" />
-          </div>
-        </div>
-        <div className="shopcategory-product">
-          {all_product.map((item, i) => {
-            if (props.category === item.category) {
-              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            }
-            else {
-              return null;
-            }
-          })}
+        <img className="shopcategory-banner" src={props.banner} alt="" />
         </div>
       </div>
-  )
-}
-
-export default ShopCategory
