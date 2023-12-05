c0860d4218bdcd51d178ee81c048fc1b41fa7b4d
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-05 20:27:28 +0330

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
 
-const product = () => {
+const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} =useParams();
+  const product =all_product.find((e)=>e.id === Number(productId));
   return (
-    <div>product</div>
+    <div>
+      <Breadcrums product={product}/>
+    </div>
   )
 }
 
-export default product
+export default Product
