aa93d94ace0e24bf5f88410fc69d416fc0684705
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-06 20:27:47 +0330

    Update ProductDisplay.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -29,6 +29,26 @@ const ProductDisplay = (props) => {
                 <img src={star_dull_icon} alt="" />
                 <p>(122)</p>
             </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-prices-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                A lightweight, usually knitted, pullover shirt, close-fitting , A lightweight, usually knitted, pullover shirt, close-fitting , A lightweight, usually knitted, garment. 
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-size">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt , Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
 
     </div>
