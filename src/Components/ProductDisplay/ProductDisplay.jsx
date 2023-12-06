1bb46a69385112f77552168fc793306482cff8cf
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-06 20:19:41 +0330

    Update ProductDisplay.jsx

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,1 +1,38 @@
-f
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from "../Assests/star_icon.png"
+import star_dull_icon from "../Assests/star_dull_icon.jpg"
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+            </div>
+            <div className="productdisplay-main-img">
+                <img src={product.ima
+                } alt="" />
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(122)</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default ProductDisplay
