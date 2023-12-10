5ee1182e02f2dfb7614316e4c701ec3f0f22a3e3
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-10 20:05:33 +0330

    Update DescriptionBox.jsx

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,1 +1,20 @@
-c
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionBox'>
+            <div className="descriptionBox-navigator">
+                <div className="descriptionBox-nav-box">Description</div>
+                <div className="descriptionBox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div class='descriptionBox-discription'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit numquam assumenda cupiditate, eius blanditiis odio eveniet iusto non reiciendis voluptate deleniti perspiciatis a ad temporibus aut? Consequuntur, aspernatur. Amet, nemo magnam? Porro delectus tempore eligendi molestiae totam illum quia consequuntur architecto, impedit veritatis vel! Alias, facere tenetur sed doloribus asperiores eum nisi inventore nemo esse optio aut explicabo? Minima perferendis dolores, quidem, rerum quaerat sapiente, harum modi quisquam delectus iure maxime! Autem quibusdam nostrum, vitae cumque quas minima ullam illo officia id hic omnis exercitationem fuga aliquam dolores amet expedita. Voluptatem at nam, perspiciatis deserunt corporis, eligendi, quos provident earum ad excepturi iste vero quam impedit reiciendis optio similique velit? Aperiam eveniet autem omnis, aliquam odit quisquam laudantium saepe quae maiores magni quis libero at natus sit, placeat labore voluptatibus perspiciatis modi dignissimos dicta repellat dolore officiis. Sunt, harum? Praesentium, hic accusantium temporibus aut odio sed perferendis error molestias vel!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, laborum pariatur architecto incidunt porro earum corrupti beatae praesentium exercitationem, accusamus autem, officia harum voluptatum dolorem! Corporis reprehenderit dignissimos aut provident architecto doloremque vitae ex sapiente, perferendis voluptatem nisi beatae distinctio eligendi tenetur! Error velit non eos at quibusdam, commodi adipisci.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
