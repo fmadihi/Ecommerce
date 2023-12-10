2c926a0cb7f021939a79e5ba87406e2bf68b8931
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-10 20:14:19 +0330

    Create RelatedProduct.jsx

---
+++ src/Components/RelatedProduct/RelatedProduct/RelatedProduct.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './RelatedProduct.css'
+import data_product from '../Assests/data'
+
+const RelatedProduct = () => {
+  return (
+    <div className='relatedProduct'>
+        <h1>Related Poducts</h1>
+        <hr />
+        <div className="relatedProduct-item">
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default RelatedProduct
