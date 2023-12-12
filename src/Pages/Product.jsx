1896cdf2905b3628f0486582c5772c64facd75d7
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-12-13 01:00:27 +0330

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Breadcrums from '../Components/Breadcrums/Breadcrums';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProduct from '../Components/RelatedProduct/RelatedProduct';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -14,6 +15,7 @@ const Product = () => {
       <Breadcrums product={product}/>
       <ProductDisplay product={product}/>
       <DescriptionBox />
+      <RelatedProduct/>
     </div>
   )
 }
